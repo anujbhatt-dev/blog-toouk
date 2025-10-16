47466826c8cdfde8d7f55c95cab2e2086519b059
anujbhatt-dev <anujbhatt023@gmail.com>
2025-10-16 15:43:09 +0400

    fix: global assettype

--- store/globalStore.ts
+++ store/globalStore.ts
@@ -1,29 +1,16 @@
-import { AssetType, ChainType } from '@/utils/types'
+
 import { create } from 'zustand'
 
 type Store = {
   registerPortal: boolean
   loginPortal: boolean
-  selectedCountry: "USA" | "CHINA" | "UAE" | "ALL"
-  selectedChain: ChainType | "ALL"
-  selectedAsset: AssetType | "ALL"
   setRegisterPortal: (v: boolean) => void
   setLoginPortal: (v: boolean) => void
-  selectCountry: (c: "USA" | "CHINA" | "UAE" | "ALL") => void
-  selectChain: (c: ChainType | "ALL") => void
-  setSelectedAsset: (a: AssetType | "ALL") => void
 }
 
 export const useGlobalStore = create<Store>()((set) => ({
   registerPortal: false,
   loginPortal: false,
-  selectedCountry: "UAE",
-  selectedChain: "ALL",
-  selectedAsset: "ALL",
-
   setRegisterPortal: (v) => set({ registerPortal: v }),
   setLoginPortal:    (v) => set({ loginPortal: v }),
-  selectCountry:     (c) => set({ selectedCountry: c }),
-  selectChain:       (c) => set({ selectedChain: c }),
-  setSelectedAsset:  (a) => set({ selectedAsset: a }),
 }))
